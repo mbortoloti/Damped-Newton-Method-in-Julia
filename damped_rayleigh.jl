e83c0220c6269fb47f403a7589262748f2be1d5a
mbortoloti <mbortoloti@uesb.edu.br>
2022-02-03 21:35:56 -0300

    updating files

---
+++ damped_rayleigh.jl
@@ -0,0 +1,21 @@
+#
+#   Damped Newton Method for solving Rayleigh quotient problem
+#
+
+function damped_rayleigh(x,maxiter,ϵ)
+
+    iter = 0;
+    info_error = 0;
+
+    println("Iteration      || ∇f(x) || ");
+    while true
+        ng = norm(gradf(x));
+        @printf("%5d      %18.14e\n",iter,ng);
+        if ng < ϵ
+            println("Solution was found!");
+            return x;
+        end
+        return x;
+    end
+
+end
--- main_rayleigh.jl
+++ main_rayleigh.jl
@@ -5,6 +5,9 @@
 
 using Random;
 using LinearAlgebra;
+using Printf;
+
+include("damped_rayleigh.jl");
 
 # Dimension setting
     n = 2;
@@ -14,7 +17,7 @@ using LinearAlgebra;
     rng = MersenneTwister(1234);
     A = randn(rng,n,n);
     A = A' * A;
-    A = A + n * Matrix{Float64}(I, n, n);
+    A = A + n * I;
     
 # Rayleigh quotient definition
     f(x) = - x' * A * x;
@@ -23,10 +26,22 @@ using LinearAlgebra;
     ∇f(x) = -2.0 * A * x;
 
 # Orthogonal projection function
-    proj(u,x) = (Matrix{Float64}(I, n, n) - x * x') * u;
+    proj(u,x) = (I - x * x') * u;
 
 # Riemannian gradient of f
     gradf(x) = proj(∇f(x),x);
 
+# Retraction setting
+    function ret(x,v)
+        xpv = x + v;
+        return xpv / norm(xpv);
+    end
 
+
+
+    maxiter = 100;
+    ϵ = 1.e-7;
+    x0 = rand(n);
+
+    damped_rayleigh(x0,maxiter,ϵ);
 
