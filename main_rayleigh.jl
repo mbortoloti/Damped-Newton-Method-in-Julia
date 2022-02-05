16f48bad3f453912919c108555b42acfbac1f7da
mbortoloti <mbortoloti@uesb.edu.br>
2022-02-05 09:15:26 -0300

    Update main_rayleigh.jl

--- main_rayleigh.jl
+++ main_rayleigh.jl
@@ -2,6 +2,13 @@
 #    Damped Newton Method for solving 
 #    The Rayleigh Quotienthe on the Sphere
 #
+#
+#   Bortoloti, M. A. A., Fernandes, T. A., Ferreira, 
+#   An efficient damped Newton-type algorithm with globalization strategy on Riemannian manifolds, 
+#   Journal of Computational and Applied Mathematics, Volume 403, (2022).
+#
+#
+
 
 using Random;
 using LinearAlgebra;
@@ -10,7 +17,7 @@ using Printf;
 include("damped_rayleigh.jl");
 
 # Dimension setting
-    n = 100;
+    n = 10;
 
 # Symmetric positive definite matrix setting
     rng = MersenneTwister(1234);
@@ -43,7 +50,7 @@ include("damped_rayleigh.jl");
 
     maxiter = 100;
     ϵ = 1.e-7;
-    x0 = rand(n);
+    x0 = rand(rng,n);
 
     iter,x,info =  damped_rayleigh(x0,maxiter,ϵ);
 
